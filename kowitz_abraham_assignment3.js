700445d02cc42b10124f9903a3953f4c422599c0
Abrham Kowitz <stormsurfin@fullsail.edu>
2012-05-14 07:13:59 -0600

    Added Projet 3 Beginning to the repo

---
+++ kowitz_abraham_assignment3.js
@@ -0,0 +1,26 @@
+//SDI Project 3
+//Abraham Kowitz
+//May 13, 2012
+
+//Global Variables
+
+var json = {
+	"hikers": [
+	{
+		name: "Janina",
+		title: "Girlfriend",
+		gear: "camelback", "fruit", "kind bars", "warm clothes", "yaktracks", "sunglasses"
+	},
+	{
+		name: "Abe",
+		title: "Boyfriend",
+		gear: "camelback", "soup", "stove", "warm clothes", "yaktracks", "sunglasses"
+	},
+	{
+		name: "Riley",
+		title: "Dog",
+		gear: ""
+	}
+	]
+};
+	
