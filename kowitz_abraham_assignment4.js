680847bd16c3f71767ba4e593348d0c528f28201
Abrham Kowitz <stormsurfin@fullsail.edu>
2012-05-23 20:58:38 -0600

    update to the repo

--- kowitz_abraham_assignment4.js
+++ kowitz_abraham_assignment4.js
@@ -1,14 +1,15 @@
 // Detecting a phone number
 
 var phone = "980-980-0098";
-function validate (phone) {
+function validate(phone) {
     var regex = /^([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$/;
 	    if (regex.test(phone)) {
 	    console.log("Valid phone number.")
 		} else {
-		console.log("Invalid phone number.")
-	}
-};
+		console.log("Invalid phone number.");
+	};
+}
+
 	
 //  Detecting an email address
 
@@ -26,11 +27,11 @@ function detectEmail(){
 
 function ValidURL(str) {
   var pattern = new RegExp('^(https?:\/\/)?'+  //checks for http:// or https://
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ 
-    '((\d{1,3}\.){3}\d{1,3}))'+ 
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ 
-    '(\?[;&a-z\d%_.~+=-]*)?'+ 
-    '(\#[-a-z\d_]*)?$','i'); 
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ //checks for domain name sequence
+    '((\d{1,3}\.){3}\d{1,3}))'+ // checks for ip address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port number or path detection
+    '(\\?[;&a-z\d%_.~+=-]*)?'+  // detects a query string
+    '(\#[-a-z\d_]*)?$','i'); // recognizes # and its referring fragment
   if(!pattern.test(str)) {
     console.log("Not a valid URL.");
     return false;
@@ -38,6 +39,12 @@ function ValidURL(str) {
     console.log("This is a valid web address.");
     return true;
   }
+};
+
+// Title case a string
+
+function toTitleCase(str){
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
 // Replace characters in a string
@@ -49,7 +56,7 @@ console.log(s);
 // Format a number to a specific number of decimal places
 
 var n = 566.8337;
-var twoDecimal = function (){
+var twoDecimal = function(){
 	var n = number.toFixed(2);  // uses .tofixed to round to hundredths
 	console.log("Converted " + twoDecimal + "to two decimal places.");
 	};
@@ -57,17 +64,17 @@ var twoDecimal = function (){
 	
 //  Fuzzy match percentages
 
-Number.prototype.isFuzzy= function(compare, percent){ //isFuzzy function compares a number and by what percentage
-    var dev= compare*(percent/100), n= +this; // math equation to make the number a percentage
-    return n>= compare-dev && n<= compare+dev;
+Number.prototype.isFuzzy = function(compare, percent){ // isFuzzy function compares a number and by what percentage
+    var dev = compare*(percent/100), n = +this; // math equation to make the number a percentage
+    return n >= compare-dev && n <= compare+dev;
 };
-var n= 151; // number to compare to
+var n = 151; // number to compare to
 console.log(n.isFuzzy(140,10)); // number compared, percentage
 
-// Number of hours between two dates
+// Number of days between two dates
 
-var startdate = 12/31/2000,
-	enddate = 05/25/2012;
+var startDate = 12/31/2000,
+	endDate = 05/25/2012;
 function dateDiff(startDate, endDate) {
     return endDate.getDate() - startDate.getDate();
     console.log(dateDiff);
@@ -80,8 +87,8 @@ console.log(parseFloat(stringNum));
 
 // Smallest value in an array that is greater than 9
 
-var min=9;
-var r=[4, 7, 12, 24];
+var min = 9;
+var r = [4, 7, 12, 24];
 function gt(a){return a>this; }
 
 console.log(
@@ -99,16 +106,19 @@ var temp = val.split(" ");
 var total = 0;
 var v;
 for(var i = 0; i < temp.length; i++) {
-v = parseFloat(temp[i]);
-if (!isNaN(v)) total += v;
+	v = parseFloat(temp[i]);
+	if (!isNaN(v)) total += v;
 }
 
 document.getElementById('resultSum').innerHTML=total;
 
 }
 
-// Sort an array of objects by a key value
+// Sort an array of objects by a key's value
 
 var array=[655, 77, 190, 1, 2004]
 console.log(array.sort(function(x,y){return x - y})); 
-// the array is sorted based on the relationship between each pair of elements and the return value.
+
+/* the array is sorted based on the relationship 
+between each pair of elements and the return value.*/
+
